3948a533356d4c28d5d47eaaa4443799988a6f43
MathSquared <MathSquared@users.noreply.github.com>
2015-04-05 01:30:20 -0500

    HDB.containsKey1 and containsKey2

--- NationalsPointTracker/src/mathsquared/pointtrack/HashDoubleMap.java
+++ NationalsPointTracker/src/mathsquared/pointtrack/HashDoubleMap.java
@@ -21,4 +21,35 @@ public class HashDoubleMap <K, L, V> {
 	public void clear () {
 		hash.clear();
 	}
+	
+	/**
+	 * Tests for whether this doublemap contains a mapping with the given object as its first key.
+	 * 
+	 * This doesn't just check if there is a sub-map corresponding to {@code key}, since that sub-map might be empty. This only returns true if that sub-map exists and is not empty.
+	 * 
+	 * @param key the key to check
+	 * @return true if there is at least one mapping with {@code key} as the first key
+	 */
+	public boolean containsKey1 (K key) {
+		// Can't naively test the parent map in case of empty sub-maps
+		
+		HashMap<L, V> comp = hash.get(key);
+		return (comp != null && !comp.isEmpty());
+	}
+	
+	/**
+	 * Tests for whether this doublemap contains a mapping with the given object as its second key.
+	 * 
+	 * This is <strong>slow</strong> since it must iterate over all of the first keys.
+	 * 
+	 * @param key the key to check
+	 * @return true if there is at least one mapping with {@code key} as the second key
+	 */
+	public boolean containsKey2 (L key) {
+		for (HashMap<L, V> comp : hash.values()) {
+			if (comp.containsKey(key)) return true;
+		}
+		
+		return false;
+	}
 }
